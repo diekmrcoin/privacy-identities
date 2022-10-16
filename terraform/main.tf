2f8b07733e084617ceffaf9063af7353712d6385
Diego Maroto <diego.diemg@gmail.com>
2022-10-16 16:30:37 +0200

    Adding first terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "1.3.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.34"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
